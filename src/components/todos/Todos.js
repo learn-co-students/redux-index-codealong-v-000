6abaeb99b4d3f262a5c1469e650813d2ce8e253d
MargaritaKhodzka <mkhodzka@gmail.com>
2017-11-17 22:44:39 -0500

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
 
 class Todos extends Component {
 
   render() {
-
+//tell each individual Todo about the text that it is rendering
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <li key={index}>{todo.text}</li>
+      return <Todo text={todo.text} key={index} />
     });
 
     return(
