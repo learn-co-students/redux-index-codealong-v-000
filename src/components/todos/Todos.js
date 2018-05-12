e620de45d01017212dc080315f780ed71e92a43a
brittmmendez <brittmmendez@gmail.com>
2018-05-12 00:37:12 -0400

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
+
+//we have passed our store to the Todos component as a prop. Then we access the array of todos by calling store.getState(), and calling map to return an array of React elements, each of type li.
 
 class Todos extends Component {
 
   render() {
 
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <li key={index}>{todo.text}</li>
+      return <Todo text={todo.text} key={index} />
     });
 
     return(
