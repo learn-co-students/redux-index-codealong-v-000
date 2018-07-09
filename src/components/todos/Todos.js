7f874009c2a614464833f398e118651239455d0e
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-07-09 17:50:15 -0500

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -5,7 +5,7 @@ class Todos extends Component {
   render() {
 
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <li key={index}>{todo.text}</li>
+      return <Todo key={index} text={todo.text} />
     });
 
     return(
