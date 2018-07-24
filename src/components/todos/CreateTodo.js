9a9df8dd602ed9630c6e47082b60d9f404b01dc0
Canino Developments <ian.canino@gmail.com>
2018-07-23 20:16:10 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -30,14 +30,15 @@ class CreateTodo extends Component {
         <form onSubmit={(event) => this.handleSubmit(event)}>
           <p>
             <label>add todo</label>
-            <input type="text" onChange={(event) => this.handleChange(event)} />
+            <input
+              type="text"
+              value={this.state.text} /* <- add value here */
+              onChange={(event) => this.handleChange(event)} />
           </p>
           <input type="submit" />
         </form>
-        {this.state.text}
       </div>
     );
   }
-};
 
 export default CreateTodo;
