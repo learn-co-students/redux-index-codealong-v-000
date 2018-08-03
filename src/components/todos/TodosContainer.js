153cfda00aa61456034931c6f3a7f71c897e75b7
illusioninlost <rchang9999@yahoo.com>
2018-08-03 15:10:54 -0700

    Done.

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+
+class TodosContainer extends Component{
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return{
+        todos: state.todos
+    }
+}
+export default connect(mapStateToProps)(TodosContainer);
