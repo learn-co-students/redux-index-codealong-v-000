108d13fbc4e1c7d7f17c9ef4fe9cb976f62ae9f5
github username <github email address>
2018-11-11 15:40:09 -0700

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,7 +3,7 @@ export default function manageTodo(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
-
+      
       return { todos: state.todos.concat(action.payload.text) };
 
     default:
