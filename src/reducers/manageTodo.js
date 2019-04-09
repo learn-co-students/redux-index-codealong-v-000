65f14ba0de68b419f09f60cb45a789c7f527a093
ElleeB <laura.trager@gmail.com>
2019-04-09 10:12:35 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -4,9 +4,9 @@ export default function manageTodo(state = {
   switch (action.type) {
     case 'ADD_TODO':
 
-      return { todos: state.todos.concat(action.payload.text) };
+      return { todos: state.todos.concat(action.payload.text) }
 
     default:
-      return state;
+      return state
   }
 }
